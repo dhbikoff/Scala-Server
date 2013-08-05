25d9571e3cc0614d73edea22e5b05a82648b1428
dhbikoff <dhbikoff@gmail.com>
2013-08-05 11:58:29 -0400

    Fixed program flow from router to buildResponse.
    Improved some variable names. Fixed jpeg/jpg pattern match in
    contentType.

--- SimpleServer.scala
+++ SimpleServer.scala
@@ -2,8 +2,19 @@ import java.io.{File, FileInputStream, IOException, OutputStream}
 import java.net.{ServerSocket, Socket, SocketException}
 import scala.io.{BufferedSource, Codec}
 
-class ConResponse(clientSocket: Socket) extends Runnable {
-    
+object SimpleServer {
+  def main(args: Array[String]) = {
+    val serverSocket = new ServerSocket(8000)
+    while(true) {
+      val clientSocket = serverSocket.accept
+      val response = new Thread(new ConResponse(clientSocket))
+      response.start()
+    }
+    serverSocket.close
+  }
+}  
+
+class ConResponse(clientSocket: Socket) extends Runnable {    
   case class ResponseHeader(
     status: String, 
     date: String = "Date: " + new java.util.Date,
@@ -19,22 +30,22 @@ class ConResponse(clientSocket: Socket) extends Runnable {
 
   def contentType(path: String): String = {
     val ext = (path split ('.')).last
-    val ct = "Content-Type: "
+    val content = "Content-Type: "
     val typeName = ext match {
       case "css" => "text/" + ext
       case "js" => "text/javascript"
       case "gif" | "png" => "image/" + ext
-      case "jpg" => "image/jpeg"
+      case "jpg" | "jpeg" => "image/jpeg"
       case "pdf" => "application/" + ext
       case "mp4" | "avi" | "mov" | "m4v" | 
         "ogv" | "flv" | "webm" | "asf" => "video/" + ext
       case "txt" => "text/plain"
       case _ => "text/html; charset=UTF-8"
     }
-    ct + typeName
+    content + typeName
   }
 
-  def header(fileName: String, found: Boolean): ResponseHeader = {
+  def buildHeader(fileName: String, found: Boolean): ResponseHeader = {
     val header = {
       if (found) {
         val ctype = contentType(fileName) 
@@ -46,31 +57,31 @@ class ConResponse(clientSocket: Socket) extends Runnable {
     header
   }
 
-  def router(input: String): (ResponseHeader, BufferedSource) = {
-    val route = {
-      val split = input.split(" ")
-      val s = if (split.length > 1) split(1) else "/"
-      if (s == "/") "/index.html"
-      else s
-    }
-    response(route)
+  def router(input: String): String = {
+    val split = input.split(" ")
+    val filename = if (split.length > 1) split(1) else "/"
+    if (filename == "/") "/index.html"
+    else filename
   }
 
-  def response(filename: String) = {
+  def buildResponse(input: String): (ResponseHeader, BufferedSource) = {
+    val filename = router(input)
     val path = "public/" + filename
-    val found = new File(path).exists
-    val head = header(filename, found)
-    val src = {
+    val found = (new File(path)).isFile
+    val header = buildHeader(filename, found)
+    val body = {
       if (found) {
+        // 200 OK
         val in = new FileInputStream(path)
         new BufferedSource(in)(Codec.ISO8859)
       }
       else {
+        // 404 NOT FOUND
         val in = new FileInputStream("public/error.html")
         new BufferedSource(in)(Codec.ISO8859)
       }
     }
-    (head, src)
+    (header, body)
   }
 
   def sendResponseBody(fileStream: BufferedSource, outStream: OutputStream) = {
@@ -85,30 +96,24 @@ class ConResponse(clientSocket: Socket) extends Runnable {
   def run() { 
     val in = clientSocket.getInputStream
     while(in.available < 1) {} // wait for request
+
+    // read request
     val buffer = new Array[Byte](in.available)
     in.read(buffer)
     val request = new String(buffer)
     println("-------REQUEST--------\n" + request)
-    val output = router(request)
-    val header = output._1
-    val fileStream = output._2
+
+    // build response
+    val response = buildResponse(request)
+    val header = response._1
+    val body = response._2
     println("-------RESPONSE-------\n" + header.toString)
+
+    // send response
     val out = clientSocket.getOutputStream
     out.write(header.getBytes)
-    sendResponseBody(fileStream, out)
+    sendResponseBody(body, out)
     clientSocket.close() // closes in and out streams
   }
 }
-
-object SimpleServer {
-  def main(args: Array[String]) = {
-    val serverSocket = new ServerSocket(8000)
-    while(true) {
-      val clientSocket = serverSocket.accept
-      val resp = new Thread(new ConResponse(clientSocket))
-      resp.start()
-    }
-    serverSocket.close
-  }
-}  
 
